c266694ffeff9142a5735b9f51a9873aacb1efed
Martina Lumastro <martinalumastro99@gmail.com>
2024-07-14 16:14:05 +0200

    snack 6 correzione

--- js/snack-06.js
+++ js/snack-06.js
@@ -4,9 +4,10 @@ console.log('snack 6')
 // Fate generare un numero random da 0 a 10 al computer, e chiedete all’utente di inserire un suo numero. Se il numero scelto dall’utente è uguale al numero del computer informate l’utente che ha vinto, altrimenti ha perso.
 
 
-const numPc = Math.round(Math.random() * 10)
-console.log(numPc)
+// const numPc = Math.round(Math.random() * 10)
+// console.log(numPc)
 
+const numPc = Math.floor(Math.random() * 11) //Math.round non ha la stessa probalilità per i numeri agli estremi (0 e 10) rispetto agli altri
 
 const numUtente = parseInt(prompt('Inserisci un numero da 0 a 10'))
 console.log(numUtente)
