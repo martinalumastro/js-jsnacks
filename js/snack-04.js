f0f01c33ce728ba1e17a3ebb38febbb91d7e4c98
Martina Lumastro <martinalumastro99@gmail.com>
2024-07-14 15:38:58 +0200

    snack 04 correzione (2 modo)

--- js/snack-04.js
+++ js/snack-04.js
@@ -9,9 +9,17 @@ console.log(nomi)
 let nomeUtente = prompt('Inserisci qui il tuo nome'); //String | Null
 //console.log(nomeUtente)
 
-if(nomi.includes(nomeUtente) == true) {
-    console.log('Ciao ' + nomeUtente + '. Puoi partecipare alla festa!')
-} else {
-    console.log('Ciao ' + nomeUtente + '. Mi dispiace ma non risulti tra gli invitati alla festa')
-}
+// if(nomi.includes(nomeUtente) == true) {
+//     console.log('Ciao ' + nomeUtente + '. Puoi partecipare alla festa!')
+// } else {
+//     console.log('Ciao ' + nomeUtente + '. Mi dispiace ma non risulti tra gli invitati alla festa')
+// }
 
+const trovato = nomi.includes(nomeUtente); //boolean
+//console.log(trovato)
+
+if(trovato === true) {
+    console.log('Sei tra gli invitati alla festa')
+} else {
+    console.log('Mi dispiace non sei stato invitato')
+}
