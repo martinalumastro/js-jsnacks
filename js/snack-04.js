46b9472aa6960f9611a42aec20e7d0da4cb08f10
Martina Lumastro <martinalumastro99@gmail.com>
2024-07-14 15:33:21 +0200

    snack 04

---
+++ js/snack-04.js
@@ -0,0 +1,17 @@
+console.log('snack 04')
+
+// snack 04
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+let nomi = ['Maria','Luca','Giovanni','Simone','Filippo','Elisa','Giulia','Angelina','Antonio']; //Array
+console.log(nomi)
+
+let nomeUtente = prompt('Inserisci qui il tuo nome'); //String | Null
+//console.log(nomeUtente)
+
+if(nomi.includes(nomeUtente) == true) {
+    console.log('Ciao ' + nomeUtente + '. Puoi partecipare alla festa!')
+} else {
+    console.log('Ciao ' + nomeUtente + '. Mi dispiace ma non risulti tra gli invitati alla festa')
+}
+
