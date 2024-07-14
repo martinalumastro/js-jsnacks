56161ecbb0b5d93a2292edce2f9a6f4ba035a934
Martina Lumastro <martinalumastro99@gmail.com>
2024-07-14 16:11:43 +0200

    #

--- js/snack-06.js
+++ js/snack-06.js
@@ -4,8 +4,9 @@ console.log('snack 6')
 // Fate generare un numero random da 0 a 10 al computer, e chiedete all’utente di inserire un suo numero. Se il numero scelto dall’utente è uguale al numero del computer informate l’utente che ha vinto, altrimenti ha perso.
 
 
-const numPc = Math.floor(Math.random() * 10)
+const numPc = Math.round(Math.random() * 10)
 console.log(numPc)
+
 
 const numUtente = parseInt(prompt('Inserisci un numero da 0 a 10'))
 console.log(numUtente)
